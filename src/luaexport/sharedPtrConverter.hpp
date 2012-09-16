7604c41a3e62d4dcfada881befeec2c9ed54d812
Christian Neumüller <cn00@gmx.at>
2012-09-16 18:04:52 +0200

    Make shared_ptr and luabind work together for VS11.
    Because luabind tries to find get_pointer by ADL, it does matter if shared_ptr is
    actually defined in std::tr1 and only brought to std by the using keyword.
    VS11 does the right thing and defines it directly in std.
    
    This implementation of get_pointer is #ifdefed, to work on both VS10 and 11.

--- src/luaexport/sharedPtrConverter.hpp
+++ src/luaexport/sharedPtrConverter.hpp
@@ -2,12 +2,18 @@
 #define SHAREDPTR_CONVERTER_HPP_INCLUDED SHAREDPTR_CONVERTER_HPP_INCLUDED
 
 // Non standard conforming: add function to ::std::tr1
-namespace std { namespace tr1 {
+namespace std {
+#if defined(_MSC_VER) && _MSC_VER < 1700
+namespace tr1 {
+#endif
 template<class T>
 T * get_pointer(shared_ptr<T> const& p) { return p.get(); }
 
 template<class T>
 T const* get_pointer(shared_ptr<T const> const& p) { return p.get(); }
-} } // namespace std::tr1
+#if defined(_MSC_VER) && _MSC_VER < 1700
+} // namespace tr1
+#endif
+} // namespace std
 
 #endif
