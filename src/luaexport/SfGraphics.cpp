00e5d90398d01a75c20a51cf793763bcb62af3c4
Christian Neumüller <cn00@gmx.at>
2012-11-14 16:37:18 +0100

    sfGraphics lexp: +RectangleShape, CircleShape
    Also export the Shape base class.
    Also fix any color-properties which were returned as const reference by using
    luabind's copy-policy.
    Also add addDrawableDefs() and addTextureProp() to improve code reuse.

--- src/luaexport/SfGraphics.cpp
+++ src/luaexport/SfGraphics.cpp
@@ -11,6 +11,7 @@ static char const libname[] = "SfGraphics";
 #include "ExportThis.hpp"
 #include "sharedPtrConverter.hpp"
 #include <luabind/operator.hpp>
+#include <luabind/copy_policy.hpp>
 #include <ostream>
 
 // sf::String <-> Lua converter (see luabind/detail/policy.hpp:741)
@@ -90,6 +91,27 @@ static void addResMngMethods(luabind::class_<T, std::shared_ptr<T>, B >& c)
 #undef F
 }
 
+template <typename T, typename B>
+static void addDrawableDefs(luabind::class_<T, B >& c)
+{
+    c
+        .def(luabind::constructor<>())
+        .def(luabind::constructor<T const&>())
+        .def(luabind::constructor<TransformGroup&>())
+        .property("group", &T::group, &T::setGroup)
+        .property("localBounds", &T::getLocalBounds)
+        .property("bounds", &T::getGlobalBounds);
+}
+
+template <typename T, typename B>
+static void addTextureProp(luabind::class_<T, B >& c)
+{
+    c.property("texture",
+        (T::Ptr(T::*)())&T::resource,
+        &T::setResource);
+}
+
+
 std::ostream& operator<< (std::ostream& o, sf::Color c)
 {
     return o << "jd.Color("
@@ -159,6 +181,13 @@ static void Text_resetStyle(sf::Text& text)
     text.setStyle(sf::Text::Regular);
 }
 
+static sf::Vector2f Shape_point(sf::Shape const& s, unsigned idx)
+{
+    if (idx >= s.getPointCount())
+        throw "point index of shape out of range";
+    return s.getPoint(idx);
+}
+
 typedef AutoResource<sf::Sprite, sf::Texture> AutoSprite;
 typedef GroupedDrawable<AutoSprite> SpriteEntry;
 
@@ -167,6 +196,11 @@ typedef GroupedDrawable<TransformGroup> GroupEntry;
 typedef AutoResource<sf::Text, VFileFont> AutoText;
 typedef GroupedDrawable<AutoText> TextEntry;
 
+typedef AutoResource<sf::RectangleShape, sf::Texture> AutoRectangleShape;
+typedef GroupedDrawable<AutoRectangleShape> RectangleShapeEntry;
+
+typedef AutoResource<sf::CircleShape, sf::Texture> AutoCircleShape;
+typedef GroupedDrawable<AutoCircleShape> CircleShapeEntry;
 
 
 static void init(LuaVm& vm)
@@ -211,6 +245,54 @@ static void init(LuaVm& vm)
 #   undef LHCURCLASS
     addResMngMethods(cFont);
 
+#   define LHCURCLASS SpriteEntry
+    class_<LHCURCLASS, bases<Sprite, TransformGroup::AutoEntry>> cSprite("Sprite");
+    cSprite
+        .property("textureRect",
+            &LHCURCLASS::getTextureRect, &LHCURCLASS::setTextureRect)
+        .property("color",
+            &LHCURCLASS::getColor, &LHCURCLASS::setColor, copy(result));
+#   undef LHCURCLASS
+    addDrawableDefs(cSprite);
+    addTextureProp(cSprite);
+
+#   define LHCURCLASS RectangleShapeEntry
+        class_<LHCURCLASS, bases<RectangleShape, TransformGroup::AutoEntry>> cRectangleShape("RectangleShape");
+        cRectangleShape
+            .property("size", &LHCURCLASS::getSize, &LHCURCLASS::setSize);
+#   undef LHCURCLASS
+    addDrawableDefs(cRectangleShape);
+    addTextureProp(cRectangleShape);
+
+#   define LHCURCLASS CircleShapeEntry
+        class_<LHCURCLASS, bases<CircleShape, TransformGroup::AutoEntry>> cCircleShape("CircleShape");
+        cRectangleShape
+            .property("radius", &LHCURCLASS::getRadius, &LHCURCLASS::setRadius)
+            .property("pointCount", &LHCURCLASS::getPointCount, &LHCURCLASS::setPointCount);
+#   undef LHCURCLASS
+    addDrawableDefs(cRectangleShape);
+    addTextureProp(cRectangleShape);
+
+#   define LHCURCLASS TextEntry
+        class_<LHCURCLASS, bases<Text, TransformGroup::AutoEntry>> cText("Text");
+        cText
+            .property("font",
+                (LHCURCLASS::Ptr(LHCURCLASS::*)())&LHCURCLASS::resource,
+                &LHCURCLASS::setResource)
+            .property("string",
+                &LHCURCLASS::getString, &LHCURCLASS::setString)
+            .property("characterSize",
+                &LHCURCLASS::getCharacterSize, &LHCURCLASS::setCharacterSize)
+            .property("bold", &Text_isBold, &Text_setBold)
+            .property("italic", &Text_isItalic, &Text_setItalic)
+            .property("underlined", &Text_isUnderlined, &Text_setUnderlined)
+            .def("isRegular", &Text_isRegular)
+            .def("resetStyle", &Text_resetStyle)
+            .LHMEMFN(findCharacterPos)
+            .property("color",
+                &LHCURCLASS::getColor, &LHCURCLASS::setColor, copy(result));
+#   undef LHCURCLASS
+    addDrawableDefs(cText);
 
 #define XYPROP(n, N) \
     .property(BOOST_STRINGIZE(n), \
@@ -268,54 +350,42 @@ static void init(LuaVm& vm)
         cTexture,
         class_<Font>("@Font@"),
         cFont,
-        class_<TransformGroup, bases<Transformable, Drawable>>("@TranformGroup@"),
+        class_<TransformGroup, bases<Transformable, Drawable>>("@TranformGroup@"),  
+
+#       define LHCURCLASS GroupEntry
 		class_<GroupEntry, bases<TransformGroup, TransformGroup::AutoEntry>>("TransformGroup")
-            .def(constructor<TransformGroup&>()),
+            .def(constructor<>())
+            .def(constructor<TransformGroup&>())
+            .property("group", &LHCURCLASS::group, &LHCURCLASS::setGroup),
+#       undef LHCURCLASS
 
 		class_<Sprite, bases<Drawable, Transformable>>("@Sprite@"),
-#       define LHCURCLASS SpriteEntry
-        class_<LHCURCLASS, bases<Sprite, TransformGroup::AutoEntry>>("Sprite")
-            .def(constructor<>())
-            .def(constructor<LHCURCLASS const&>())
-            .def(constructor<TransformGroup&>())
-            .property("group", &LHCURCLASS::group, &LHCURCLASS::setGroup)
-            .property("texture",
-                (LHCURCLASS::Ptr(LHCURCLASS::*)())&LHCURCLASS::resource,
-                &LHCURCLASS::setResource)
+        cSprite,
+        
+        class_<Text, bases<Drawable, Transformable>>("@Text@"),
+        cText,
+
+#       define LHCURCLASS Shape
+        class_<LHCURCLASS, bases<Drawable, Transformable>>("Shape") 
+            .property("localBounds", &LHCURCLASS::getLocalBounds)
+            .property("bounds", &LHCURCLASS::getGlobalBounds)
+            .property("fillColor",
+                &LHCURCLASS::getFillColor, &LHCURCLASS::setFillColor, copy(result))
+            .property("outlineColor",
+                &LHCURCLASS::getOutlineColor, &LHCURCLASS::setOutlineColor, copy(result))
+            .property("outlineThickness",
+                &LHCURCLASS::getOutlineThickness, &LHCURCLASS::setOutlineThickness)
+            .property("pointCount", &LHCURCLASS::getPointCount)
+            .def("point", &Shape_point)
             .property("textureRect",
-                &LHCURCLASS::getTextureRect, &LHCURCLASS::setTextureRect)
-            .property("color",
-                &LHCURCLASS::getColor, &LHCURCLASS::setColor)
-            .property("localBounds", &LHCURCLASS::getLocalBounds)
-            .property("bounds", &LHCURCLASS::getGlobalBounds),
+                &LHCURCLASS::getTextureRect, &LHCURCLASS::setTextureRect),
 #       undef LHCURCLASS
+        
+        class_<RectangleShape, Shape>("@RectangleShape@"),
+        cRectangleShape,
 
-		class_<Text, bases<Drawable, Transformable>>("@Text@"),
-#       define LHCURCLASS TextEntry
-        class_<LHCURCLASS, bases<Text, TransformGroup::AutoEntry>>("Text")
-            .def(constructor<>())
-            .def(constructor<LHCURCLASS const&>())
-            .def(constructor<TransformGroup&>())
-            .property("group", &LHCURCLASS::group, &LHCURCLASS::setGroup)
-            .property("font",
-                (LHCURCLASS::Ptr(LHCURCLASS::*)())&LHCURCLASS::resource,
-                &LHCURCLASS::setResource)
-            .property("string",
-                &LHCURCLASS::getString, &LHCURCLASS::setString)
-            .property("characterSize",
-                &LHCURCLASS::getCharacterSize, &LHCURCLASS::setCharacterSize)
-            .property("bold", &Text_isBold, &Text_setBold)
-            .property("italic", &Text_isItalic, &Text_setItalic)
-            .property("underlined", &Text_isUnderlined, &Text_setUnderlined)
-            .def("isRegular", &Text_isRegular)
-            .def("resetStyle", &Text_resetStyle)
-            .LHMEMFN(findCharacterPos)
-            .property("color",
-                &LHCURCLASS::getColor, &LHCURCLASS::setColor)
-            .property("localBounds", &LHCURCLASS::getLocalBounds)
-            .property("bounds", &LHCURCLASS::getGlobalBounds),
-#       undef LHCURCLASS
-
+        class_<CircleShape, Shape>("@CircleShape@"),
+        cCircleShape,
 
 #       define LHCURCLASS View
         LHCLASS
