fafe0c4a59a5a96b23cc34e54849adbd8fd2714e
Christian Neumüller <cn00@gmx.at>
2012-11-14 16:35:42 +0100

    AutoResource: Support sf::Shape.

--- src/AutoTexture.hpp
+++ src/AutoTexture.hpp
@@ -3,11 +3,17 @@
 
 #include "AutoResource.hpp"
 #include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Shape.hpp>
 #include <SFML/Graphics/Texture.hpp>
 
 void updateAutoResourceMedia(sf::Sprite& s, ResourceTraits<sf::Texture>::Ptr tx)
 {
     s.setTexture(*tx, true);
+}
+
+void updateAutoResourceMedia(sf::Shape& s, ResourceTraits<sf::Texture>::Ptr tx)
+{
+    s.setTexture(tx.get(), true);
 }
 
 #endif
