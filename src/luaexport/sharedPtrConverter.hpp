b7d7effbd76242a9e95d293eca280d903251a720
Christian Neumüller <cn00@gmx.at>
2013-03-02 16:15:35 +0100

    Adjust to new Boost version 1.53.
    The <boost/get_pointer.hpp> header now also supports std::shared_pointer and
    std::unique_pointer. This lead to ambiguous calls in some places in luabind.
    
    The new version of sharedPtrConverter.hpp now pulls the definitions directly
    in the luabind::detail::has_get_pointer_ namespace, so that luabind continues
    to recognize std::shared_pointer as a smart pointer.
    
    Via #if, sharedPtrConverter.hpp still works on Boost 1.52 and below.

--- src/luaexport/sharedPtrConverter.hpp
+++ src/luaexport/sharedPtrConverter.hpp
@@ -1,10 +1,24 @@
 #ifndef SHAREDPTR_CONVERTER_HPP_INCLUDED
 #define SHAREDPTR_CONVERTER_HPP_INCLUDED SHAREDPTR_CONVERTER_HPP_INCLUDED
 
+#include <boost/version.hpp>
+
+
+#if BOOST_VERSION >= 105300
+
+#include <boost/get_pointer.hpp>
+
+
+namespace luabind { namespace detail { namespace has_get_pointer_ {
+    using boost::get_pointer;
+}}}
+
+#else // if BOOST_VERSION < 105300
+
 #include <memory>
 
 
-// Non standard conforming: add function to ::std::tr1
+// Not standard conforming: add function to ::std(::tr1)
 namespace std {
 #if defined(_MSC_VER) && _MSC_VER < 1700
 namespace tr1 {
@@ -18,5 +32,7 @@ T const* get_pointer(shared_ptr<T const> const& p) { return p.get(); }
 } // namespace tr1
 #endif
 } // namespace std
+
+#endif // if BOOST_VERSION < 105300
 
 #endif
